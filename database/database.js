32dab862ae2f3826dbc01582c7f6141b67cee487
Victor <haillothere@gmail.com>
2023-01-05 16:08:38 -0300

    Corrigindo db

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://Victor:Welcome22@cluster0.waa6iem.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
